77992e2dfa54dc85849b67b80855202229066f53
PseudoFunc <sudipta0102@gmail.com>
2024-04-22 01:49:58 +0530

    first checkin here

---
+++ src/main/java/com/rest/Main.java
@@ -0,0 +1,15 @@
+package com.rest;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Employee emp = new Employee();
+		
+		emp.setName("me").setAge(49);
+		
+		System.out.println(emp.toString());
+
+	}
+
+}
---
+++ src/main/java/com/rest/Organization.java
@@ -0,0 +1,23 @@
+package com.rest;
+
+public class Organization {
+	
+	String orgName;
+	int empId;
+	
+	public String getOrgName() {
+		return orgName;
+	}
+	public void setOrgName(String orgName) {
+		this.orgName = orgName;
+	}
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	
+	
+
+}
